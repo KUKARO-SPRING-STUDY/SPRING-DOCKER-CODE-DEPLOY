0d2127c00c8cddcd5a45a15d5e42089726386e34
coeat.k <coeat.k@kakaocorp.com>
2024-04-16 03:57:18 +0900

    feat: health 경로 배재

--- src/main/java/groom/backend/springtricount/config/webmvcconfigurer/WebConfig.java
+++ src/main/java/groom/backend/springtricount/config/webmvcconfigurer/WebConfig.java
@@ -13,12 +13,13 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
     private final LoginCheckInterceptor loginCheckInterceptor;
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginCheckInterceptor)
                 .order(1)
                 .addPathPatterns("/**")
-                .excludePathPatterns("/api/auth/signup", "/api/auth/login", "/h2-console/**");
+                .excludePathPatterns("/api/auth/signup", "/api/auth/login", "/h2-console/**", "/health");
     }
 
     @Override
